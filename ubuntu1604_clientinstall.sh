a5a5f9d5b5add2f17ba520d0f5c92da7e6e8f0f2
Tom Monte <tmonte@gmail.com>
2017-10-13 16:40:44 -0400

    Create ubuntu1604_clientinstall.sh

---
+++ ubuntu1604_clientinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo wget https://apt.puppetlabs.com/puppet5-release-xenial.deb
+sudo dpkg -i puppet5-release-xenial.deb
+sudo apt-get update
+sudo apt-get puppet-agent
+sudo systemctl start puppet-agent
+sudo systemctl enable puppet-agent
