289c889dd4ed9d01ec48ffbddd9745c604beb187
Tom Monte <tmonte@gmail.com>
2017-10-15 16:09:00 -0400

    Update puppetserverinstall.sh

--- puppetserverinstall.sh
+++ puppetserverinstall.sh
@@ -3,6 +3,8 @@
 # Ubuntu 16.04
 # Puppet 5
 # server install
+# Puppet server requires at least 4GB of ram to run smoothly.
+# Interesting note that Ansible requires very little ram to run smoothly.
 
 sudo wget https://apt.puppetlabs.com/puppet5-release-xenial.deb
 sudo dpkg -i puppet5-release-xenial.deb
