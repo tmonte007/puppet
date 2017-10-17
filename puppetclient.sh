d0578782ca96e479f04b89ba46d0ac5ff1a4b2b6
Tom Monte <tmonte@gmail.com>
2017-10-16 20:32:59 -0400

    Update puppetclient.sh

--- puppetclient.sh
+++ puppetclient.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# install puppet
 sudo apt -y install ruby
 sudo gem install puppet
+
+# test puppet
 touch hello.pp
 chmod +x hello.pp
 # ' require '\'' to escape them
 echo -e 'notify { '\''Hello world\!'\'':' >> hello.pp
 echo -e '}' >> hello.pp
 puppet apply hello.pp
+
+# Connect to the server
+puppet config set --section agent server puppetmaster.monte.network
+puppet agent --test
