4917234df4fad6fcd11d24ec74b914a310f342b3
Tom Monte <tmonte@gmail.com>
2017-10-17 20:11:25 -0400

    Update adduser.pp

--- adduser.pp
+++ adduser.pp
@@ -2,8 +2,6 @@ node default { }
 
 user { 'puppetadmin':
   ensure     => present,
-  uid        => '1000',
-  gid        => '1000',
   shell      => '/bin/bash',
   home       => '/home/puppetadmin'
 }
